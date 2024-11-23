fc7a24426bb3ce99a24de6d2ed713acd16434608
MrGITit <46465169+MrGITit@users.noreply.github.com>
2024-11-23 17:20:31 -0600

    Apply updated .gitignore rules

--- obj/Release/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SVS_PostHClothingStatePersistence
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SVS_PostHClothingStatePersistence";
-        public const string PLUGIN_NAME = "SVS Post H Clothing State Persistence";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
